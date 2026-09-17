444251c272a5600e5f95b05e26c7f6fef372eeb3
talalscorpions-dotcom <talalscorpions-dotcom@users.noreply.github.com>
2026-09-17 09:15:26 +0000

    deploy: f891ed7ced3905293d85b91382ecc7d1f12b91ff

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "0d7b17b1b48df003fe24e40b6384f843",
 "version.json": "3f584f3244e90844433d6a51579f377d",
-"main.dart.js": "6f3c205b82535b542b08fa5bf9750457",
-"flutter_bootstrap.js": "2b021530b8782903414305ee8451fec4",
+"main.dart.js": "f6d62c22576f273262cee5d5f7aa1100",
+"flutter_bootstrap.js": "50524bb3dfcbcc73040f591b56ce0a46",
 "icons/Icon-maskable-512.png": "4ba888e70edf39e9b6bdf383d2a6d8f6",
 "icons/Icon-192.png": "4bb08f8cd8e3922249b54bbcea3713da",
 "icons/Icon-maskable-192.png": "dfa92b9a3c68768f5cb4c405c8235a0f",
